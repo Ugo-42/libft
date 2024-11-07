8c710857c4734c26019a522822689365ece89345
Ugo Wentzinger <ugwentzi@k1r2p12.42mulhouse.fr>
2024-11-07 11:17:03 +0100

    norminette

--- ft_split.c
+++ ft_split.c
@@ -6,30 +6,11 @@
 /*   By: ugwentzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 10:37:02 by ugwentzi          #+#    #+#             */
-/*   Updated: 2024/10/28 09:58:30 by ugwentzi         ###   ########.fr       */
+/*   Updated: 2024/11/07 11:16:36 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	ft_count_words(const char *str, char c)
-{
-	int	words;
-
-	words = 0;
-	while (*str)
-	{
-		if (*str != c)
-		{
-			words++;
-			while (*str && *str != c)
-				str++;
-		}
-		if (*str)
-			str++;
-	}
-	return (words);
-}
 
 static void	ft_strncpy(char *dest, const char *src, int n)
 {
