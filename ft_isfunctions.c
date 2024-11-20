356a0b8802ac207014ceb119ac759ba2c1ac0fef
Ugo Wentzinger <ugwentzi@k1r2p12.42mulhouse.fr>
2024-11-20 10:43:26 +0100

    put every is functions is on ft_isfunctions.c file

--- ft_isfunctions.c
+++ ft_isfunctions.c
@@ -1,16 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isfunctions.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ugwentzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/14 14:13:22 by ugwentzi          #+#    #+#             */
-/*   Updated: 2024/10/22 11:00:27 by ugwentzi         ###   ########.fr       */
+/*   Created: 2024/11/20 10:39:10 by ugwentzi          #+#    #+#             */
+/*   Updated: 2024/11/20 10:41:33 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	ft_isalnum(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+		|| (c >= '0' && c <= '9'));
+}
 
 int	ft_isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+int	ft_isascii(int c)
+{
+	return (c >= 0 && c <= 127);
+}
+
+int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int	ft_isprint(int c)
+{
+	return (c >= 32 && c < 127);
 }
