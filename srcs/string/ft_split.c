f91c2257563d0b4f3186a60219e1a1f620426346
0xK92JL4 <157831311+0xK92JL4@users.noreply.github.com>
2024-11-28 23:12:37 +0100

    better split

--- srcs/string/ft_split.c
+++ srcs/string/ft_split.c
@@ -1,46 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ugwentzi <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/26 09:23:46 by ugwentzi          #+#    #+#             */
-/*   Updated: 2024/11/26 09:23:47 by ugwentzi         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft_string.h"
 #include "libft_allocation.h"
+#include "libft_string.h"
 #include "libft_count.h"
 
-static char	**ft_malloc_word(char **result, int r, const char *w, int word_len)
+static void	ft_free_result(char **result, int r)
 {
-	result[r] = malloc((word_len + 1) * sizeof(char));
-	if (!result[r])
-	{
-		while (r >= 0)
-			free(result[r--]);
-		free(result);
-		return (NULL);
-	}
-	ft_strncpy(result[r], w, word_len);
-	return (result);
+	while (r >= 0)
+		free(result[r--]);
+	free(result);
 }
 
-char	**ft_split(const char *str, char c)
+static int	ft_split_words(char **result, const char *str, const char c)
 {
-	char	**result;
-	int		word_len;
-	int		i;
-	int		r;
-
-	if (!str)
-		return (NULL);
-	result = ft_calloc((ft_count_words(str, c) + 1), sizeof(char *));
-	if (!result)
-		return (NULL);
-	r = -1;
+	size_t	word_len;
+	size_t	r;
+	size_t	i;
+	
+	r = 0;
 	i = 0;
 	while (str[i])
 	{
@@ -49,10 +24,34 @@ char	**ft_split(const char *str, char c)
 		word_len = 0;
 		while (str[i + word_len] && str[i + word_len] != c)
 			word_len++;
-		if (word_len > 0)
-			if (!ft_malloc_word(result, ++r, &str[i], word_len))
-				return (NULL);
+		result[r] = malloc((word_len + 1) * sizeof(char));
+		if (!result[r])
+		{
+			ft_free_result(result, r);
+			return (0);
+		}
+		ft_strncpy(result[r], &str[i], word_len);
 		i += word_len;
+		r++;
 	}
+	return (1);
+}
+
+char    **ft_split(const char *str, const char c)
+{
+	char	**result;
+	int		words;
+
+	if (!str)
+		return (NULL);
+	words = ft_count_words(str, c);
+	if (!words)
+		return (NULL);
+  	result = malloc((words + 1) * sizeof(char *));
+	if (!result)
+		return (NULL);
+	if (!ft_split_words(result, str, c))
+		return (NULL);
+	result[words] = NULL;
 	return (result);
 }
