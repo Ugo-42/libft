7160e490bbf613cf2cac318e67018217c1786c71
Ugo Wentzinger <ugwentzi@k1r2p12.42mulhouse.fr>
2024-11-29 14:50:27 +0100

    some edits

--- srcs/string/ft_strncpy.c
+++ srcs/string/ft_strncpy.c
@@ -6,19 +6,23 @@
 /*   By: ugwentzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 09:23:58 by ugwentzi          #+#    #+#             */
-/*   Updated: 2024/11/26 09:23:59 by ugwentzi         ###   ########.fr       */
+/*   Updated: 2024/11/29 14:50:15 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_strncpy(char *dest, const char *src, int n)
+#include <stddef.h>
+
+void	ft_strncpy(char *dest, const char *src, size_t n)
 {
-	int	i;
+	size_t	i;
 
 	if (!dest || !src)
 		return ;
 	i = 0;
-	while (*src && i < n)
-		dest[i++] = *src++;
-	while (i <= n)
-		dest[i++] = '\0';
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 }
