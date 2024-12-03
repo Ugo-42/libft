22edb5cf1c8a0db3f1c393ed4e81d991a4f82530
Ugo Wentzinger <ugwentzi@k1r2p12.42mulhouse.fr>
2024-12-03 11:05:42 +0100

    some edits

--- includes/libft_string.h
+++ includes/libft_string.h
@@ -6,7 +6,7 @@
 /*   By: ugwentzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:58:18 by ugwentzi          #+#    #+#             */
-/*   Updated: 2024/11/29 16:06:46 by ugwentzi         ###   ########.fr       */
+/*   Updated: 2024/12/02 10:25:05 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
-char    *ft_str_ultimate_join(const char *first, ...);
-char    *ft_strdup(const char *s);
+char	*ft_str_ultimate_join(const char *first, ...);
+char	*ft_strdup(const char *s);
 
 void	ft_strcpy(char *dest, const char *src);
 void	ft_strncpy(char *dest, const char *src, int n);
