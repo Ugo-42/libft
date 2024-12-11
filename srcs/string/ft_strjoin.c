fd473d821af92a584798508a49bd83171892aba5
Ugo Wentzinger <ugwentzi@k1r2p12.42mulhouse.fr>
2024-12-11 10:34:36 +0100

    some edits

--- srcs/string/ft_strjoin.c
+++ srcs/string/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ugwentzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 09:23:53 by ugwentzi          #+#    #+#             */
-/*   Updated: 2024/11/26 09:23:54 by ugwentzi         ###   ########.fr       */
+/*   Updated: 2024/12/11 10:34:32 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*result;
 
-	if (!s1)
-		return ((char *)s2);
-	if (!s2)
-		return ((char *)s1);
+	if (!s1 || !s2)
+		return (NULL):
 	result = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!result)
 		return (NULL);
