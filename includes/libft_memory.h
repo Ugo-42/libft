30e35adb3216dc3beebc4b8a6d04ad5fe0a4395c
Ugo Wentzinger <ugwentzi@k1r2p12.42mulhouse.fr>
2024-12-20 10:24:26 +0100

    some edits

--- includes/libft_memory.h
+++ includes/libft_memory.h
@@ -6,7 +6,7 @@
 /*   By: ugwentzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:58:18 by ugwentzi          #+#    #+#             */
-/*   Updated: 2024/12/12 16:16:19 by ugwentzi         ###   ########.fr       */
+/*   Updated: 2024/12/20 10:24:16 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	ft_swap_address(void **a, void **b);
+void	ft_swap_char_address(char **a, char **b);
 
 #endif
