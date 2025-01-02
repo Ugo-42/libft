0a1ba20066b36a7a13d2af3051dffd420a575890
0xK92JL4 <157831311+0xK92JL4@users.noreply.github.com>
2025-01-02 23:42:13 +0100

    added type_sizes.h

---
+++ includes/type_sizes.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   type_sizes.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ugwentzi <ugwentzi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 10:51:56 by ugwentzi          #+#    #+#             */
+/*   Updated: 2024/12/20 12:23:04 by ugwentzi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TYPE_SIZES_H
+# define TYPE_SIZES_H
+
+/* Basic Types */
+# define SIZE_CHAR			sizeof(char)
+# define SIZE_SHORT			sizeof(short)
+# define SIZE_INT			sizeof(int)
+# define SIZE_LONG			sizeof(long)
+# define SIZE_LONG_LONG		sizeof(long long)
+
+/* Floating Types */
+# define SIZE_FLOAT			sizeof(float)
+# define SIZE_DOUBLE		sizeof(double)
+# define SIZE_LONG_DOUBLE	sizeof(double)
+
+/* Pointer Type */
+# define SIZE_PTR			sizeof(void *)
+
+#endif
