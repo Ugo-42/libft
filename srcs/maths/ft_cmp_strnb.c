2d7f54c5bded825fe93e157c9fcef7f935044b68
Ugo Wentzinger <ugwentzi@student.42mulhouse.fr>
2025-01-29 11:29:36 +0100

    up cmp_strnb

--- srcs/maths/ft_cmp_strnb.c
+++ srcs/maths/ft_cmp_strnb.c
@@ -6,37 +6,56 @@
 /*   By: ugwentzi <ugwentzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 11:54:00 by ugwentzi          #+#    #+#             */
-/*   Updated: 2025/01/23 09:54:25 by ugwentzi         ###   ########.fr       */
+/*   Updated: 2025/01/29 11:28:37 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_cmp_strnb(const char *nb1, const char *nb2)
+static bool	ft_is_negative(const char *nb)
+{
+	return (nb[0] == '-');
+}
+
+static int	ft_compare_negatives(const char *nb1, const char *nb2)
 {
 	size_t	len_nb1;
 	size_t	len_nb2;
 
 	len_nb1 = ft_strlen(nb1);
 	len_nb2 = ft_strlen(nb2);
-	if (nb1[0] == '-' && nb2[0] != '-')
+	if (len_nb1 > len_nb2)
 		return (-1);
-	if (nb1[0] != '-' && nb2[0] == '-')
+	if (len_nb1 < len_nb2)
 		return (1);
-	if (nb1[0] == '-' && nb2[0] == '-')
-	{
-		if (len_nb1 > len_nb2)
-			return (-1);
-		if (len_nb1 < len_nb2)
-			return (1);
-		return (ft_strcmp(nb2 + 1, nb1 + 1));
-	}
+	return (ft_strcmp(nb2 + 1, nb1 + 1));
+}
+
+static int	ft_compare_positives(const char *nb1, const char *nb2)
+{
+	size_t	len_nb1;
+	size_t	len_nb2;
+
+	len_nb1 = ft_strlen(nb1);
+	len_nb2 = ft_strlen(nb2);
+	if (len_nb1 > len_nb2)
+		return (1);
+	if (len_nb1 < len_nb2)
+		return (-1);
+	return (ft_strcmp(nb1, nb2));
+}
+
+int	ft_cmp_strnb(const char *nb1, const char *nb2)
+{
+
+	if (!nb1 || !nb2)
+		ft_exit_error(1, "In 'ft_cmp_strnb': " BAD_ARG, false);
+	if (ft_is_negative(nb1) && !ft_is_negative(nb2))
+		return (-1);
+	if (!ft_is_negative(nb1) && ft_is_negative(nb2))
+		return (1);
+	if (ft_is_negative(nb1) && ft_is_negative(nb2))
+		return (ft_compare_negatives(nb1, nb2));
 	else
-	{
-		if (len_nb1 > len_nb2)
-			return (1);
-		if (len_nb1 < len_nb2)
-			return (-1);
-		return (ft_strcmp(nb1, nb2));
-	}
+		return (ft_compare_positives(nb1, nb2));
 }
