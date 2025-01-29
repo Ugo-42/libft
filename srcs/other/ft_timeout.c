fc269b05799c2e9fdef3d6c67eba0eb123bd8e47
Ugo Wentzinger <ugwentzi@student.42mulhouse.fr>
2025-01-29 16:55:15 +0100

    added ft_timeout

--- srcs/other/ft_timeout.c
+++ srcs/other/ft_timeout.c
@@ -1,23 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_other.h                                      :+:      :+:    :+:   */
+/*   ft_timeout.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ugwentzi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ugwentzi <ugwentzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/14 16:58:18 by ugwentzi          #+#    #+#             */
-/*   Updated: 2025/01/29 16:52:13 by ugwentzi         ###   ########.fr       */
+/*   Created: 2025/01/29 16:38:08 by ugwentzi          #+#    #+#             */
+/*   Updated: 2025/01/29 16:54:54 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_OTHER_H
-# define LIBFT_OTHER_H
+#include "libft.h"
+#include "color.h"
 
-/*┌────────┐
-  │ other/ │
-  └────────┘*/
+void	ft_timeout(int max_calls)
+{
+	static int	timeout;
 
-void	ft_segfault(void);
-void	ft_timeout(int max_calls);
-
-#endif
+	if (timeout++ == max_calls)
+	{
+		ft_putstr(BOLD TOP_LOW_LINE RED "Timeout\n" RESET_COLOR);
+		exit(0);
+	}
+}
