91b5ad7936457e33fb560ff4dddc1f1e83ddcc7c
0xK92JL4 <157831311+0xK92JL4@users.noreply.github.com>
2025-02-06 20:43:01 +0100

    some edits 🗿

--- srcs/count/ft_utf8_strlen.c
+++ srcs/count/ft_utf8_strlen.c
@@ -16,13 +16,21 @@ size_t	ft_utf8_strlen(const char *str)
 {
 	size_t	len;
 
-	len = 0;
 	if (!str)
 		ft_exit_error(1, "In 'ft_utf8_strlen': " BAD_ARG, 0);
+	len = 0;
 	while (*str)
 	{
 		if ((*str & 0xC0) != 0x80)
+		{
 			len++;
+			if ((*str & 0xF0) == 0xF0)
+				str += 3;
+			else if ((*str & 0xE0) == 0xE0)
+				str += 2;
+			else if ((*str & 0xC0) == 0xC0)
+				str += 1;
+		}
 		str++;
 	}
 	return (len);
