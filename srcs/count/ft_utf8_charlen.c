6d8c8e997a2ebb32e63b43cb3854a83c40f17afb
Ugo Wentzinger <ugwentzi@student.42mulhouse.fr>
2025-02-07 15:26:26 +0100

    some edits

--- srcs/count/ft_utf8_charlen.c
+++ srcs/count/ft_utf8_charlen.c
@@ -6,7 +6,7 @@
 /*   By: ugwentzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 09:22:05 by ugwentzi          #+#    #+#             */
-/*   Updated: 2025/02/07 14:32:17 by ugwentzi         ###   ########.fr       */
+/*   Updated: 2025/02/07 15:22:47 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t	ft_utf8_charlen(const char *wide_c)
 	byte = (unsigned char)wide_c[0];
 	if ((byte & 0x80) == 0)
 		return (1);
-	if ((byte & 0xE0) != 0xC0)
+	if ((byte & 0xE0) == 0xC0)
 		return (2);
 	if ((byte & 0xF0) == 0xE0)
 		return (3);
