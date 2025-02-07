a0ce3c9b58f4a2832188f87e6a3d91daab6dba0a
Ugo Wentzinger <ugwentzi@student.42mulhouse.fr>
2025-02-07 10:49:57 +0100

    normed

--- includes/libft_is.h
+++ includes/libft_is.h
@@ -6,7 +6,7 @@
 /*   By: ugwentzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:58:18 by ugwentzi          #+#    #+#             */
-/*   Updated: 2025/01/21 13:40:35 by ugwentzi         ###   ########.fr       */
+/*   Updated: 2025/02/07 10:49:47 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@
 
 bool	ft_is_emoji(const char *wide_c);
 
-int	ft_is_alnum(int c);
-int	ft_is_alpha(int c);
-int	ft_is_ascii(int c);
-int	ft_is_digit(int c);
-int	ft_is_print(int c);
-int	ft_is_quote(int c);
-int	ft_is_space(int c);
+int		ft_is_alnum(int c);
+int		ft_is_alpha(int c);
+int		ft_is_ascii(int c);
+int		ft_is_digit(int c);
+int		ft_is_print(int c);
+int		ft_is_quote(int c);
+int		ft_is_space(int c);
 
 #endif
