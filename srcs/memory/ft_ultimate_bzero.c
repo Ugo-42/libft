a76dc5371f9bf76661bf83e66c71683a5ffd4a8c
0xK92JL4 <157831311+0xK92JL4@users.noreply.github.com>
2025-02-21 23:56:18 +0100

    up ult bzero.c

--- srcs/memory/ft_ultimate_bzero.c
+++ srcs/memory/ft_ultimate_bzero.c
@@ -14,13 +14,12 @@
 
 /*
  ' initialize every passed variable to zero:
- '   if type[0] == '%'
- '     the the following type will be treated for all passed variables.
- '   else
- '     you need to separate each type with a comma ','.
- ' Exemples:
- '   ("%i", (int)&a, (int)&b, (int)&c)
- '   ("i,c,zu,d", (int)&a, (char)&b, (size_t)&c, (double)&d)
+ '   > you need to separate each type with a comma ','.
+ '   Exemple:
+ '     ("i,c,zu,d", (int)&a, (char)&b, (size_t)&c, (double)&d)
+ '   > you can put nb before the type to repeat same type.
+ '   Exemple:
+ '     ("3i,2p", (int)&a, (int)&b, (int)&c, (void *)&d, (void *)&e)
 */
 
 static size_t	ft_type_size(const char *type)
@@ -70,7 +69,7 @@ static bool	ft_valid_type(const char *type)
 	return (false);
 }
 
-static int	process_format(const char *type, va_list *args)
+static int	process_format(const char *type, int count, va_list *args)
 {
 	void	*var;
 
@@ -79,33 +78,44 @@ static int	process_format(const char *type, va_list *args)
 		ft_warning("In 'ft_ultimate_bzero': Invalid Format");
 		return (-1);
 	}
-	var = va_arg(*args, void *);
-	if (!var)
+	while (count-- > 0)
 	{
-		ft_warning("In 'ft_ultimate_bzero': " BAD_ARG);
-		return (-1);
+		var = va_arg(*args, void *);
+		if (!var)
+		{
+			ft_warning("In 'ft_ultimate_bzero': " BAD_ARG);
+			return (-1);
+		}
+		ft_bzero(var, ft_type_size(type));
 	}
-	ft_bzero(var, ft_type_size(type));
 	return (0);
 }
 
 void	ft_ultimate_bzero(const char *formats, ...)
 {
 	va_list	args;
-	char	**types;
+	char	**tokens;
+	char	*token;
+	int		count;
 	size_t	i;
 
-	types = ft_split(formats, ',');
-	if (!types)
+	tokens = ft_split(formats, ',');
+	if (!tokens)
 		ft_exit_error(1, "In 'ft_ultimate_bzero': " BAD_ALLOC);
 	va_start(args, formats);
 	i = 0;
-	while (types[i])
+	while (tokens[i])
 	{
-		if (process_format(types[i], &args) != 0)
-			break ;
+		count = 1;
+		token = tokens[i];
+		while (ft_is_digit(*token))
+			token++;
+		if (token != tokens[i])
+			count = atoi(tokens[i]);
+		if (process_format(token, count, &args) != 0)
+			break;
 		i++;
 	}
 	va_end(args);
-	ft_free_2d_char_array(&types);
+	ft_free_2d_char_array(&tokens);
 }
