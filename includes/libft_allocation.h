0992687986559f5ac9e3735ba949eb3f7be7c5e2
Ugo Wentzinger <ugwentzi@student.42mulhouse.fr>
2025-02-27 12:31:03 +0100

    some edits

--- includes/libft_allocation.h
+++ includes/libft_allocation.h
@@ -6,7 +6,7 @@
 /*   By: ugwentzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:58:18 by ugwentzi          #+#    #+#             */
-/*   Updated: 2025/02/27 11:32:10 by ugwentzi         ###   ########.fr       */
+/*   Updated: 2025/02/27 12:30:59 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
   │ allocation/ │
   └─────────────┘*/
 
-void	**ft_malloc_2d(size_t cols, size_t rows, size_t size);
+void	**ft_malloc_2d(size_t dim1, size_t dim2, size_t size);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_free_2d_array(void ***array, size_t n);
 void	*ft_free_2d_char_array(char ***array);
--- srcs/allocation/ft_malloc_2d.c
+++ srcs/allocation/ft_malloc_2d.c
@@ -6,24 +6,24 @@
 /*   By: ugwentzi <ugwentzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 11:25:13 by ugwentzi          #+#    #+#             */
-/*   Updated: 2025/02/27 11:29:06 by ugwentzi         ###   ########.fr       */
+/*   Updated: 2025/02/27 12:30:34 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	**ft_malloc_2d(size_t cols, size_t rows, size_t size)
+void	**ft_malloc_2d(size_t dim1, size_t dim2, size_t size)
 {
 	void	**array;
 	size_t	i;
 
-	array = malloc(cols * sizeof(void *));
+	array = malloc(dim1 * sizeof(void *));
 	if (!array)
 		return (NULL);
 	i = 0;
-	while (i < cols)
+	while (i < dim1)
 	{
-		array[i] = malloc(rows * size);
+		array[i] = malloc(dim2 * size);
 		if (!array[i])
 		{
 			while (i > 0)
