aaf90944ce9f2c6a4bde3756c2e2f0ae0e5c7046
0xK92JL4 <157831311+0xK92JL4@users.noreply.github.com>
2025-03-21 19:29:04 +0100

    some edits 🗿

--- includes/libft_cleanup.h
+++ includes/libft_cleanup.h
@@ -1,33 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   libft_cleanup.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ugwentzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:58:18 by ugwentzi          #+#    #+#             */
-/*   Updated: 2025/03/21 10:49:38 by ugwentzi         ###   ########.fr       */
+/*   Updated: 2025/03/21 10:49:26 by ugwentzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef LIBFT_CLEANUP_H
+# define LIBFT_CLEANUP_H
 
-# include "libft_allocation.h"
-# include "libft_cleanup.h"
-# include "libft_color.h"
-# include "libft_conversion.h"
-# include "libft_count.h"
-# include "libft_error.h"
-# include "libft_is.h"
-# include "libft_list.h"
-# include "libft_maths.h"
-# include "libft_memory.h"
-# include "libft_other.h"
-# include "libft_print.h"
-# include "libft_read.h"
-# include "libft_skip.h"
-# include "libft_sort.h"
-# include "libft_string.h"
+/*┌──────────┐
+  │ cleanup/ │
+  └──────────┘*/
+
+void	ft_cexit(int exit_code, void (*clean_ft)(void *), void *clean_arg);
 
 #endif
