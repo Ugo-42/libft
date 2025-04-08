546b9b64894eceb896fa43e8b6fcd382670685a2
0xK92JL4 <157831311+0xK92JL4@users.noreply.github.com>
2025-04-08 22:10:45 +0200

    fixed fstr_remove

--- srcs/flexible_string/flexistr_remove.c
+++ srcs/flexible_string/flexistr_remove.c
@@ -14,18 +14,16 @@
 
 int	flexistr_remove(t_flexistr *fs, size_t start, size_t end)
 {
-	size_t	remove_len;
 	size_t	new_size;
 
 	if (!fs || !fs->string)
 		return (1);
-	if (start >= fs->len || end <= start)
+	if (start >= fs->len || end < start)
 		return (0);
-	if (end > fs->len)
-		end = fs->len;
-	new_size = fs->len - (end - start);
-	remove_len = end - start;
-	ft_memmove(fs->string + start, fs->string + end, fs->len - end + 1);
+	if (end >= fs->len)
+		end = fs->len - 1;
+	new_size = fs->len - (end + 1 - start);
+	ft_memmove(fs->string + start, fs->string + end + 1, fs->len - end);
 	fs->len = new_size;
 	if (fs->len < fs->size / 4)
 	{
